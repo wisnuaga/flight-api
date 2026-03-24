808cd636ed2ca860cf94835b1c459606360e9c95
Wisnu Agastya <wisnu.aga@gmail.com>
2026-03-25 00:07:27 +0700

    init mux server

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	port := ":8080"
+	log.Printf("Server is starting and listening on port %s...\n", port)
+
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatalf("Failed to start server: %v\n", err)
+	}
 }
