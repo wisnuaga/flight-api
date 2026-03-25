a13a858bf433e359c12bfaa807b3a3bdbb220c3a
Wisnu Agastya <wisnu.aga@gmail.com>
2026-03-25 22:04:26 +0700

    init flight router

--- internal/delivery/http/router/flight_router.go
+++ internal/delivery/http/router/flight_router.go
@@ -6,6 +6,6 @@ import (
 	"github.com/wisnuaga/flight-api/internal/delivery/http/handler"
 )
 
-func RegisterFlightRoutes(mux *http.ServeMux, h *handler.FlightHandler) {
+func registerFlightRoutes(mux *http.ServeMux, h *handler.FlightHandler) {
 	mux.HandleFunc("/flights/search", h.Search)
 }
