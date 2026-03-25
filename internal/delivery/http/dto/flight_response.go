d2c2bd5d57c06d2e784582e164e3a59d5dc44330
Wisnu Agastya <wisnu.aga@gmail.com>
2026-03-25 21:42:45 +0700

    create flight response dto

--- internal/delivery/http/dto/flight_response.go
+++ internal/delivery/http/dto/flight_response.go
@@ -1,4 +1,68 @@
 package dto
 
 type SearchResponse struct {
+	SearchCriteria SearchCriteria `json:"search_criteria"`
+	Metadata       Metadata       `json:"metadata"`
+	Flights        []Flight       `json:"flights"`
+}
+
+type SearchCriteria struct {
+	Origin        string `json:"origin"`
+	Destination   string `json:"destination"`
+	DepartureDate string `json:"departure_date"`
+	Passengers    int    `json:"passengers"`
+	CabinClass    string `json:"cabin_class"`
+}
+
+type Metadata struct {
+	TotalResults       int  `json:"total_results"`
+	ProvidersQueried   int  `json:"providers_queried"`
+	ProvidersSucceeded int  `json:"providers_succeeded"`
+	ProvidersFailed    int  `json:"providers_failed"`
+	SearchTimeMs       int  `json:"search_time_ms"`
+	CacheHit           bool `json:"cache_hit"`
+}
+
+type Flight struct {
+	ID             string   `json:"id"`
+	Provider       string   `json:"provider"`
+	Airline        Airline  `json:"airline"`
+	FlightNumber   string   `json:"flight_number"`
+	Departure      Location `json:"departure"`
+	Arrival        Location `json:"arrival"`
+	Duration       Duration `json:"duration"`
+	Stops          int      `json:"stops"`
+	Price          Price    `json:"price"`
+	AvailableSeats int      `json:"available_seats"`
+	CabinClass     string   `json:"cabin_class"`
+	Aircraft       *string  `json:"aircraft"`
+	Amenities      []string `json:"amenities"`
+	Baggage        Baggage  `json:"baggage"`
+}
+
+type Airline struct {
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+type Location struct {
+	Airport   string `json:"airport"`
+	City      string `json:"city"`
+	Datetime  string `json:"datetime"`
+	Timestamp int64  `json:"timestamp"`
+}
+
+type Duration struct {
+	TotalMinutes int    `json:"total_minutes"`
+	Formatted    string `json:"formatted"`
+}
+
+type Price struct {
+	Amount   int64  `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+type Baggage struct {
+	CarryOn string `json:"carry_on"`
+	Checked string `json:"checked"`
 }
