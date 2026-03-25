749e1ef30fc50423876ba27ac975045e0916ef9f
Wisnu Agastya <wisnu.aga@gmail.com>
2026-03-26 00:34:56 +0700

    create mock loader util

---
+++ utils/mock_loader.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func LoadMock[T any](path string) (T, error) {
+	var result T
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return result, err
+	}
+
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
